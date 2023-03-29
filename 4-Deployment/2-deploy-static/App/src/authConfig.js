538a5334fa004baea5446a3bdbc5ea80e691699b
Ivan Hu <ivan.hu@honeywell.com>
2023-03-29 14:39:27 +1100

    Why is this tutorial asking me to push a client secret?

--- 4-Deployment/2-deploy-static/App/src/authConfig.js
+++ 4-Deployment/2-deploy-static/App/src/authConfig.js
@@ -1,15 +1,15 @@
 /*
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License.
- */
+* Copyright (c) Microsoft Corporation. All rights reserved.
+* Licensed under the MIT License.
+*/
 
 import { LogLevel } from "@azure/msal-browser";
 
 /**
- * Configuration object to be passed to MSAL instance on creation. 
- * For a full list of MSAL.js configuration parameters, visit:
- * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/configuration.md 
- */
+* Configuration object to be passed to MSAL instance on creation. 
+* For a full list of MSAL.js configuration parameters, visit:
+* https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/configuration.md 
+*/
 export const msalConfig = {
     auth: {
         clientId:  `${process.env["REACT_APP_AAD_APP_CLIENT_ID"]}`, // This is the ONLY mandatory field that you need to supply.
@@ -48,19 +48,19 @@ export const msalConfig = {
 };
 
 /**
- * Scopes you add here will be prompted for user consent during sign-in.
- * By default, MSAL.js will add OIDC scopes (openid, profile, email) to any login request.
- * For more information about OIDC scopes, visit: 
- * https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-permissions-and-consent#openid-connect-scopes
- */
+* Scopes you add here will be prompted for user consent during sign-in.
+* By default, MSAL.js will add OIDC scopes (openid, profile, email) to any login request.
+* For more information about OIDC scopes, visit: 
+* https://learn.microsoft.com/azure/active-directory/develop/v2-permissions-and-consent#openid-connect-scopes
+*/
 export const loginRequest = {
     scopes: []
 };
 
 /**
- * Add here the endpoints and scopes when obtaining an access token for protected web APIs. For more information, see:
- * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/resources-and-scopes.md
- */
+* Add here the endpoints and scopes when obtaining an access token for protected web APIs. For more information, see:
+* https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/resources-and-scopes.md
+*/
 export const protectedResources = {
     graphMe: {
         endpoint: "https://graph.microsoft.com/v1.0/me",
